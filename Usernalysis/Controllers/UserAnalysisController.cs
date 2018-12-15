9105c55c949759451929c7eb9a4bd09fc1388031
James Tsao <james.tsao@gmail.com>
2018-12-14 20:26:06 -0500

    Created model for UserAnalysis.

--- Usernalysis/Controllers/UserAnalysisController.cs
+++ Usernalysis/Controllers/UserAnalysisController.cs
@@ -7,7 +7,9 @@ using System.Threading.Tasks;
 using System.Text;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Usernalysis.Models;
 using Usernalysis.Models.User;
 using Usernalysis.Lib;
 
@@ -48,39 +50,32 @@ namespace Usernalysis.Controllers
                 users.Add(user);
                 //analysisOutput.AppendLine($"{user.Name.First} {user.Name.Last} - Dob: {user.Dob.Date} - Age: {user.Dob.Age}");
             }
-            analysisOutput.AppendLine();
-            var pct = Calculators.PercentageFemale(users);
-            analysisOutput.AppendLine($"Percentage female versus male: {pct * 100:F1}%");
-            pct = Calculators.PercentageFirstNameMidpoint(users);
-            analysisOutput.AppendLine($"Percentage of first names that start with A-M [{pct * 100:F1}%] versus N-Z [{100 - (pct * 100):F1}%]");
-            pct = Calculators.PercentageLastNameMidpoint(users);
-            analysisOutput.AppendLine($"Percentage of first names that start with A-M [{pct * 100:F1}%] versus N-Z [{100 - (pct * 100):F1}%]");
-            var top10Pct = (from statePct in Calculators.PercentagePeopleInState(users)
-                            select statePct).Take(10);
+            //analysisOutput.AppendLine();
+
+            var model = Analyze(users);
+            analysisOutput.AppendLine($"Percentage female versus male: {model.Gender.PercentFemale:F1}%");
+            analysisOutput.AppendLine($"Percentage of first names that start with A-M [{model.FirstName.PercentAtoM:F1}%] versus N-Z [{model.FirstName.PercentNtoZ:F1}%]");
+            analysisOutput.AppendLine($"Percentage of last names that start with A-M [{model.LastName.PercentAtoM:F1}%] versus N-Z [{model.LastName.PercentNtoZ:F1}%]");
+            var top10 = GetTopSortedFromDictionary<string, decimal>(model.StatePercentages);
             analysisOutput.AppendLine("10 most populous states and the percentage of people in the state:");
-            foreach (var state in top10Pct)
+            foreach (var state in top10)
             {
                 analysisOutput.AppendLine($"\t{state.Key} {state.Value * 100:F1}%");
             }
-
             analysisOutput.AppendLine("10 most populous female states and the percentage of total females in the state:");
-            top10Pct = (from statePct in Calculators.PercentageFemalesInState(users)
-                        select statePct).Take(10);
-            foreach (var state in top10Pct)
+            top10 = GetTopSortedFromDictionary<string, decimal>(model.FemaleStatePercentages);
+            foreach (var state in top10)
             {
                 analysisOutput.AppendLine($"\t{state.Key} {state.Value * 100:F1}%");
             }
             analysisOutput.AppendLine("10 most populous male states and the percentage of total males in the state:");
-            top10Pct = (from statePct in Calculators.PercentageMalesInState(users)
-                        select statePct).Take(10);
-            foreach (var state in top10Pct)
+            top10 = GetTopSortedFromDictionary<string, decimal>(model.MaleStatePercentages);
+            foreach (var state in top10)
             {
                 analysisOutput.AppendLine($"\t{state.Key} {state.Value * 100:F1}%");
             }
-
             analysisOutput.AppendLine("Percentage of people in the following age ranges:");
-            var ageRangePercentages = Calculators.PercentageAgeRanges(users);
-            foreach(var ageRangePercentage in ageRangePercentages)
+            foreach (var ageRangePercentage in model.AgeRangePercentages)
             {
                 analysisOutput.AppendLine($"\t{ageRangePercentage.Key} {ageRangePercentage.Value * 100:F1}%");
             }
@@ -88,5 +83,41 @@ namespace Usernalysis.Controllers
             return analysisOutput.ToString();
         }
 
+        private UserAnalysisModel Analyze(IList<UserModel> users)
+        {
+            UserAnalysisModel model = new UserAnalysisModel();
+
+            var femalePercentage = Calculators.PercentageFemale(users) * 100;
+            var malePercentage = 100 - femalePercentage;
+            model.Gender = (femalePercentage, malePercentage);
+
+            var firstNameLeftPercentage = Calculators.PercentageFirstNameMidpoint(users) * 100;
+            var firstNameRightPercentage = 100 - firstNameLeftPercentage;
+            model.FirstName = (firstNameLeftPercentage, firstNameRightPercentage);
+
+            var lastNameLeftPercentage = Calculators.PercentageLastNameMidpoint(users) * 100;
+            var lastNameRightPercentage = 100 - lastNameLeftPercentage;
+            model.LastName = (lastNameLeftPercentage, lastNameRightPercentage);
+
+            model.StatePercentages = Calculators.PercentagePeopleInState(users);
+            model.FemaleStatePercentages = Calculators.PercentageFemalesInState(users);
+            model.MaleStatePercentages = Calculators.PercentageMalesInState(users);
+            model.AgeRangePercentages = Calculators.PercentageAgeRanges(users);
+
+            return model;
+        }
+
+        private IList<KeyValuePair<T, U>> GetTopSortedFromDictionary<T, U>(IDictionary<T, U> dictionary, int topN = 10)
+        {
+            var result = new List<KeyValuePair<T, U>>();
+            var query = (from entry in dictionary
+                         orderby entry.Value descending
+                         select entry).Take(topN);
+            foreach (var entry in query)
+            {
+                result.Add(new KeyValuePair<T, U>(entry.Key, entry.Value));
+            }
+            return result;
+        }
     }
 }
---
+++ Usernalysis/Models/UserAnalysisModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Usernalysis.Models
+{
+    public class UserAnalysisModel
+    {
+        public (decimal PercentFemale, decimal PercentMale) Gender { get; set; }
+        public (decimal PercentAtoM, decimal PercentNtoZ) FirstName { get; set; }
+        public (decimal PercentAtoM, decimal PercentNtoZ) LastName { get; set; }
+        public IDictionary<string, decimal> StatePercentages { get; set; }
+        public IDictionary<string, decimal> FemaleStatePercentages { get; set; }
+        public IDictionary<string, decimal> MaleStatePercentages { get; set; }
+        public IList<KeyValuePair<string, decimal>> AgeRangePercentages { get; set; }
+    }
+}
